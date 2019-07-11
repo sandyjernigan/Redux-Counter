2a17c26c2b7b245a252ded1107c6bfd66682a65a
Sandy <sandy.jernigan@gmail.com>
2019-07-10 20:00:50 -0400

    Added mapDispatchToProps
    Not very relative in this small of an app, but seems good practice for something larger.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -51,9 +51,14 @@ const mapStateToProps = (state) => {
     };
 };
 
+const mapDispatchToProps = { 
+    increment: increment, 
+    decrement: decrement
+}
+
 // The connect function is called in order to make this component aware
 // of the rest of the redux architecture. Without this, this component
 // is only a dumb React component. We pass in all of the functions that
 // are reliant on Redux, along with the component itself, so that Redux
 // makes itself known to this component.
-export default connect(mapStateToProps, { increment, decrement })(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
